55962e49ae0304c55963e878da8db29b324a8ed4
Saqib Suleman <saqibs3291@gmail.com>
2021-02-18 00:58:34 +0500

    add async

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const searchTerm = form.element.query.value;
-    axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    console.log(res.data)
 })
